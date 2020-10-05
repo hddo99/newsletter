b69c83b407edfbd7903ed7e0d14207ce6563f19a
hddo99 <jackdo99.work@gmail.com>
2020-10-04 21:21:01 -0400

    fixed

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.post("/failure", function (req, res) {
     res.redirect("/");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running on port 3000");
 })
 
